932a4b0d25bc934c9d1dde5907fcbc4def78a733
shelep-olya <shelep.olya@gmail.com>
2024-09-15 17:51:32 +0300

    added user route handlers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, NotFoundException, Query, Delete} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -9,6 +10,30 @@ export class UsersController {
     @Post("/signup")
     createUser(@Body() body: CreateUserDto){
         this.usersService.create(body.email, body.password);
+    }
+
+    @Get("/:id")
+    findUser(@Param('id') id: string){
+       const user = this.usersService.findOne(parseInt(id));
+       if(!user){
+        throw new NotFoundException("no user with that id");
+       }
+       return user;
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string){
+        return this.usersService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id:string){
+        return this.usersService.remove(parseInt(id));
+    }
+
+    @Patch("/:id")
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+        return this.usersService.update(parseInt(id), body);
     }
 
  }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -22,7 +22,7 @@ export class UsersService {
     async update(id: number, attrs: Partial<User>){
         const user = await this.findOne(id);
         if(!user){
-            throw new Error('user not found with that id!');
+            throw new NotFoundException('user not found with that id!');
         }
         Object.assign(user, attrs);
         return this.repo.save(user);
@@ -32,7 +32,7 @@ export class UsersService {
     async remove(id: number){
      const user = await this.findOne(id); 
      if(!user){
-        throw new Error("user not found with that id!");
+        throw new NotFoundException("user not found with that id!");
      }
      return this.repo.remove(user);
     }
