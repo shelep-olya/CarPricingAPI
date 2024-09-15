9f9611314e6fe7e5ec86a91fbfe2643656d31de2
o-shelep <shelep.olha@student.uzhnu.edu.ua>
2024-09-15 16:52:10 +0300

    implemented user service with basic crud operations

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity()
@@ -12,5 +12,17 @@ export class User{
 
     @Column()
     password: string;
-   
+    
+    @AfterInsert()
+    logInsert(){
+        console.log('Inserted User with id: ', this.id);
+    }
+    @AfterRemove()
+    logRemove(){
+        console.log('Removed User with id: ', this.id);
+    }
+    @AfterUpdate()
+    logUpdate(){
+        console.log('Updated User with id: ', this.id);
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,11 +5,36 @@ import { User } from './user.entity';
  
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>){
-        }
-        create(email: string, password: string){
-            const user = this.repo.create({email, password});
-
-            return this.repo.save(user);
+    constructor(@InjectRepository(User) private repo: Repository<User>){}
+    create(email: string, password: string){
+        const user = this.repo.create({email, password});
+        return this.repo.save(user);
     }
+
+    findOne(id: number){
+        return this.repo.findOneBy({id});
+    }
+
+    find(email: string){
+        return this.repo.find({where: {email}});
+    }
+
+    async update(id: number, attrs: Partial<User>){
+        const user = await this.findOne(id);
+        if(!user){
+            throw new Error('user not found with that id!');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number){
+     const user = await this.findOne(id); 
+     if(!user){
+        throw new Error("user not found with that id!");
+     }
+     return this.repo.remove(user);
+    }
+
+
 }
