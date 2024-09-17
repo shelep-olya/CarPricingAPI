db18e4828f9deae1cbb861c2aec430e87f57c998
shelep-olya <shelep.olya@gmail.com>
2024-09-17 11:44:45 +0300

    established sessions; implemented signout & isSignedIn functionallity; added authentication guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,56 @@
-import { Body, Controller, Post, Get, Patch, Param, NotFoundException, Query, Delete, UseInterceptors} from '@nestjs/common';
+import { 
+    Body,
+    Controller,
+    Post,
+    Get,
+    Patch,
+    Param,
+    NotFoundException,
+    Query,
+    Delete,
+    Session,
+    UseGuards
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
-
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService, private authService: AuthService){}
 
+    @Get('isSignedIn')
+    @UseGuards(AuthGuard)
+    isLoggedIn(@CurrentUser() user: User){
+        return user;
+    }
+
     @Post("/signup")
-    createUser(@Body() body: CreateUserDto){
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session:any){
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signout(@Session() session: any){
+        session.userId = null;
     }
     
     @Post("/signin")
-    signin(@Body() body: CreateUserDto){
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session:any){
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
-   
+
     @Get("/:id")
     findUser(@Param('id') id: string){
        const user = this.usersService.findOne(parseInt(id));
