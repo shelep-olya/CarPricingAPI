b68b60309212db85cf7946c7d32ec0d463ae2687
shelep-olya <shelep.olya@gmail.com>
2024-09-18 10:54:40 +0300

    implemented unit testing for auth.service

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,55 @@
+import { Test } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { UsersService } from "./users.service";
+import { User } from "./user.entity";
+import { BadRequestException } from "@nestjs/common";
+
+describe("AuthService", () => {
+
+    let service: AuthService;
+    let fakeUserService: Partial<UsersService>;
+
+    beforeEach(async () => {
+    
+         // Create a fake copy of users service.
+         fakeUserService = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User),
+        };
+    
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUserService,
+                }
+            ],
+        }).compile();
+    
+        service = module.get(AuthService);
+    });
+    
+    
+    it('can create an instance of auth service', async () => {
+        expect(service).toBeDefined();
+    });
+
+    it('creates a new user with a salted and hashed password', async() => {
+        const user = await service.signup('shelep@gmail.com', 'test1234');
+        expect(user.password).not.toEqual('test1234');
+        const [salt, hash] = user.password.split(".");
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
+    });
+
+    it('throws an error if user signs up with email that is in use', async () => {
+        fakeUserService.find = () =>
+     
+          Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+          BadRequestException,
+        );
+      });
+
+});
