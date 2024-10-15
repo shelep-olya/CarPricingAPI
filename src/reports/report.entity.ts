dc9ff23a3606ec91b963eb78d29efc72f3735a82
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-10-15 11:01:18 +0300

    Update report.entity.ts

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,11 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 import { User } from "src/users/user.entity";
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne} from "typeorm";
-
 @Entity()
 export class Report{
     
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({default: false})
+    approved: boolean;
 
     @Column()
     price: number;
@@ -29,5 +31,5 @@ export class Report{
     mileage: number;
 
     @ManyToOne(() => User, (user) => user.reports)
-    user: User;
+    user: User; 
 }
